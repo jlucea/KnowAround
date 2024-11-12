8b106e458a58b53218a67d9ffbf40185d8fabdc8
Jaime Lucea <jlucea@eservicios.indra.es>
2024-11-12 17:21:25 +0100

    Initial Commit

---
+++ KnowAround/KnowAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowAroundApp.swift
+//  KnowAround
+//
+//  Created by Jaime Lucea on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct KnowAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
